48e42c9a48e7bdff52db95488e9c57a39d45cf1b
eva <>
2019-10-09 23:39:35 +0800

    添加 DAO 扫描路径

--- omg/src/main/java/com/omg/Application.java
+++ omg/src/main/java/com/omg/Application.java
@@ -1,5 +1,6 @@
 package com.omg;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.server.ConfigurableWebServerFactory;
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpStatus;
 
 @SpringBootApplication
+@MapperScan("com.omg.dao")
 public class Application {
 
     public static void main(String[] args) {
--- omg/src/main/java/com/omg/dao/BlogDAO.java
+++ omg/src/main/java/com/omg/dao/BlogDAO.java
@@ -2,7 +2,6 @@ package com.omg.dao;
 
 import com.omg.po.Blog;
 import org.apache.ibatis.annotations.Mapper;
-import org.springframework.stereotype.Component;
 
 import java.util.List;
 
