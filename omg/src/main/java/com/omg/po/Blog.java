8305244abba7975c4a45c9e1b98605e21e61aaf3
eva <>
2019-10-13 01:01:17 +0800

    1. 添加 Comment 模型 2. 更新表结构

--- omg/src/main/java/com/omg/po/Blog.java
+++ omg/src/main/java/com/omg/po/Blog.java
@@ -1,60 +1,16 @@
 package com.omg.po;
 
-public class Blog {
+import lombok.Data;
 
-    private Long blogId;
+import java.util.List;
+
+@Data
+public class Blog {
+    private Long id;
     private String title;
     private String content;
     private String contentHTML;
     private String authorName;
     private Long userId;
-
-
-    public String getContentHTML() {
-        return contentHTML;
-    }
-
-    public void setContentHTML(String contentHTML) {
-        this.contentHTML = contentHTML;
-    }
-
-    public Long getBlogId() {
-        return blogId;
-    }
-
-    public void setBlogId(Long blogId) {
-        this.blogId = blogId;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long userId) {
-        this.userId = userId;
-    }
+    private List<Comment> comments;
 }
--- omg/src/main/resources/omg.sql
+++ omg/src/main/resources/omg.sql
@@ -1,9 +1,23 @@
 CREATE DATABASE `omg` DEFAULT CHARACTER SET utf8mb4;
+
 CREATE TABLE `blog_t` (
-  `blog_id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `title` varchar(11) DEFAULT NULL,
-  `content` text DEFAULT NULL,
+  `content` text,
   `author_name` varchar(11) DEFAULT NULL,
   `user_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`blog_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `is_delete` bit(1) DEFAULT b'0',
+  `content_HTML` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE `comment_t` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `content` text,
+  `author_name` varchar(11) DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
+  `blog_id` bigint(20) DEFAULT NULL,
+  `is_delete` bit(1) DEFAULT b'0',
+  `content_HTML` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4;
