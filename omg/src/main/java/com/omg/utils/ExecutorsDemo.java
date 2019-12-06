df888849f43bc6a68862ae3b19338078b7810f08
eva <>
2019-12-06 22:56:47 +0800

    部分修改

--- omg/src/main/java/com/omg/utils/ExecutorsDemo.java
+++ omg/src/main/java/com/omg/utils/ExecutorsDemo.java
@@ -1,22 +1,24 @@
 package com.omg.utils;
 
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.concurrent.*;
 
 public class ExecutorsDemo {
 
     public static void main(String[] args) {
-        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
-                .setNameFormat("demo-poolz-%d").build();
-        ExecutorService pool = new ThreadPoolExecutor(5, 200,
-                0L, TimeUnit.MILLISECONDS,
-                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
-        List<Future<Long>> resultList = new ArrayList<Future<Long>>();
+        ExecutorService pool = createThreadPool("base");
         for (int i = 0; i < 10; i++) {
             pool.execute(new MyTask());
         }
+    }
+
+    public static ExecutorService createThreadPool(String name) {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat(name).build();
+        ExecutorService pool = new ThreadPoolExecutor(5, 200,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+        return pool;
     }
 
 
